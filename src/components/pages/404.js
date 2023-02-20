16ab33d76343315993ea6e2c9f7b7096e2066493
Sasha Yandola <78899037+SashaYandola@users.noreply.github.com>
2023-02-20 18:44:55 +0200

    +

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,5 +1,5 @@
 import ErrorMessage from "../errorMessages/ErrorMessage";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
   const navigate = useNavigate();
@@ -14,7 +14,7 @@ const NotFound = () => {
       <p style={{ 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px' }}>Page doesn't exist</p>
       <button
         onClick={goBack}
-        style={{ 'display': 'block', 'fontWeight': 'bold', 'fontSize': '24px', 'color': '#9f0013', 'marginTop': '40px', 'textAlign': 'center' }}>Back</button>
+        style={{ 'display': 'block', 'fontWeight': 'bold', 'fontSize': '24px', 'color': '#9f0013', 'textAlign': 'center', 'margin': '30px auto', 'padding': '10px 30px' }}>Back</button>
     </div>
   );
 };
