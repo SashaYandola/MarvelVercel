ed7f0921fb35673a87a57d887e34aa566921a54c
Sasha Yandola <78899037+SashaYandola@users.noreply.github.com>
2023-02-20 18:39:06 +0200

    +

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -22,7 +22,7 @@ const NotFound = () => {
       <ErrorMessage />
       <p style={{ 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px' }}>Page doesn't exist</p>
       <button
-        to={'#'}
+
         onClick={goBack}
         style={{ 'display': 'block', 'fontWeight': 'bold', 'fontSize': '24px', 'color': '#9f0013', 'marginTop': '40px', 'textAlign': 'center' }}>Back</button>
     </div>
